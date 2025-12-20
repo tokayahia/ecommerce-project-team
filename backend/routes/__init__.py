a452dba7e78d7bdd629ce76cc9ee4c8f5dc25221
Sara Elkholy <sosarah580@gmail.com>
2025-12-20 07:36:40 +0200

    Delete backend directory

--- backend/routes/__init__.py
+++
@@ -1,6 +0,0 @@
-
-from .auth import bp as auth_bp
-from .product import bp as product_bp
-from .cart import bp as cart_bp
-from .orders import bp as orders_bp
-from .protected import bp as protected_bp
