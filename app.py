9575528cb2dd456b3dd59569e1296c7f7946235a
Sara Elkholy <sosarah580@gmail.com>
2025-12-30 10:28:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-
+from extensions import db
+import os
 
 app = Flask(__name__)
-CORS(app)  
 
+# CORS
+CORS(app, resources={r"/*": {
+    "origins": ["http://localhost:5173", "http://127.0.0.1:5173"],
+    "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    "allow_headers": ["Content-Type", "Authorization"],
+    "supports_credentials": True
+}})
+
+# Database config
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
 
+db.init_app(app)
 
 from routes.auth import auth_bp
 from routes.product import product_bp
@@ -17,13 +25,11 @@ from routes.cart import cart_bp
 from routes.orders import orders_bp
 from routes.protected import protected_bp
 
-
 app.register_blueprint(auth_bp, url_prefix='/auth')
 app.register_blueprint(product_bp, url_prefix='/products')
 app.register_blueprint(cart_bp, url_prefix='/cart')
 app.register_blueprint(orders_bp, url_prefix='/orders')
 app.register_blueprint(protected_bp, url_prefix='/protected')
-
 
 @app.route('/')
 def home():
@@ -31,6 +37,6 @@ def home():
 
 if __name__ == "__main__":
     with app.app_context():
-        db.create_all()  
-    print("✅ Backend server is running on http://127.0.0.1:5000")
+        db.create_all()
+
     app.run(debug=True)
